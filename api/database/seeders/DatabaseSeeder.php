cfc34def472774eee4500eea9e72ea4adafff6e8
Wilder Bravo <wilderbravoc@gmail.com>
2023-08-01 21:11:51 -0500

    Agregar los seeders en un solo archivo, para una sola ejecución

--- api/database/seeders/DatabaseSeeder.php
+++ api/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(ArticlesTableSeeder::class);
+        $this->call(PersonasTableSeeder::class);
     }
 }
